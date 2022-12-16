7905a76715502bf853f92e6acc22255f70e8c938
Mauro Freire <mauromarquesmoura@yahoo.com.br>
2022-12-16 16:25:34 -0300

    PermCheck

---
+++ PermCheck/Program.cs
@@ -0,0 +1,19 @@
+﻿//First solution: https://app.codility.com/demo/results/trainingB99K82-R8B/
+//Detected time complexity: O(N)
+
+int[] Array = { 1, 1, 1, 2, 3, 4 };
+Console.WriteLine(Solution.solution(Array));
+
+class Solution
+{
+    public static int solution(int[] A)
+    {
+        HashSet<int> set = new HashSet<int>(A);
+        if(set.Count != A.Length)
+            return 0;
+
+        if(set.Count == A.Max())
+            return 1;
+        return 0;
+    }
+}
