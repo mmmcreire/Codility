83ec6c4b8b6125fca1d33c9e7ff55e7c94ea6014
Mauro Freire <mauromarquesmoura@yahoo.com.br>
2022-12-17 17:47:06 -0300

    CountDiv

---
+++ CountDiv/Program.cs
@@ -0,0 +1,20 @@
+﻿//Solution: https://app.codility.com/demo/results/trainingWV5BHN-PU4/
+//Detected time complexity: O(B-A)
+
+Console.WriteLine(Solution.solution(0, 2000000000, 1));
+
+class Solution
+{
+    public static int solution(int A, int B, int K)
+    {
+        int result = 0;
+        int size = B - A;
+
+        for(int i = 0; i <= size; i++)
+        {
+            if((A + i) % K == 0)
+                result++;
+        }
+        return result;
+    }
+}
